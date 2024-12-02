2012bee30593def4a67c8c85e931c809fb6f63d7
Nicklason <Nicklason@users.noreply.github.com>
2024-12-02 11:33:21 +0100

    feat(day2): solve part 1

---
+++ AdventOfCode.Tests/Day02.cs
@@ -0,0 +1,30 @@
+namespace AdventOfCode.Tests;
+
+public class Day02
+{
+    private readonly string _input =
+        @"
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+";
+
+    [Fact]
+    public void Solve_1()
+    {
+        var result = AdventOfCode.Day02.Solve_1(_input);
+
+        Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Solve_2()
+    {
+        var result = AdventOfCode.Day02.Solve_2(_input);
+
+        Assert.Equal(0, result);
+    }
+}
---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,68 @@
+namespace AdventOfCode;
+
+public class Day02 : BaseDay
+{
+    private readonly string _input;
+
+    public Day02()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public static int Solve_1(string input)
+    {
+        var lines = input.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+        var safe = 0;
+        foreach (var line in lines)
+        {
+            var parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var previous = int.Parse(parts[0]);
+            int direction = 1;
+
+            for (var i = 1; i < parts.Length; i++)
+            {
+                var num = int.Parse(parts[i]);
+
+                var difference = num - previous;
+                previous = num;
+                if (i == 1)
+                {
+                    if (difference == 0)
+                    {
+                        break;
+                    }
+                    direction = difference > 0 ? 1 : -1;
+                }
+
+                var abs = Math.Abs(difference);
+                if (difference * direction < 0)
+                {
+                    break;
+                }
+
+                if (abs < 1 || abs > 3)
+                {
+                    break;
+                }
+
+                if (i == parts.Length - 1)
+                {
+                    safe++;
+                }
+            }
+        }
+
+        return safe;
+    }
+
+    public static int Solve_2(string input)
+    {
+        return 0;
+    }
+
+    public override ValueTask<string> Solve_1() => new(Day02.Solve_1(_input).ToString());
+
+    public override ValueTask<string> Solve_2() => new("");
+}
